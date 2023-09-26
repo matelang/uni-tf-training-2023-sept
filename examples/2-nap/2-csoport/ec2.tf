e69682d9f8bc5e6e09f0f29f47aeb9d977c8e150
Máté Lang <langmatelaszlo@gmail.com>
2023-09-26 16:40:41 +0200

    added 2nd session -2nd day

---
+++ examples/2-nap/2-csoport/ec2.tf
@@ -0,0 +1,87 @@
+data "aws_ami" "amazon" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.2.*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon.id
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public[0].id
+  iam_instance_profile = aws_iam_instance_profile.ec2.name
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "ec2"
+  role = aws_iam_role.ec2.name
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ec2" {
+  name               = "ec2"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.ec2.name
+}
+
+#
+#resource "aws_security_group" "ec2" {
+#  name        = "ec2"
+#  vpc_id      = aws_vpc.main.id
+#
+#  ingress {
+#    description      = "TLS from VPC"
+#    from_port        = 443
+#    to_port          = 443
+#    protocol         = "tcp"
+#    cidr_blocks      = [aws_vpc.main.cidr_block]
+#    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "ec2"
+#  }
+#}
---
+++ examples/2-nap/2-csoport/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  #  access_key = ""
+  #  secret_key = ""
+}
